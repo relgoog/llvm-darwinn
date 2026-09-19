ef64c59075058072abf2c4c51847ab54dffcc847
Amaan Qureshi <git@amaanq.com>
2026-09-19 07:28:44 -0400

    darwinn: custom op through embedded context wrapper

--- mlir/lib/Dialect/Darwinn/Transforms/DwcPasses.cpp
+++ mlir/lib/Dialect/Darwinn/Transforms/DwcPasses.cpp
@@ -1518,6 +1518,8 @@ struct DwcConvertDiveVmToLlvmPass
         callee = "DiveVm_UpdateDvfsHint";
       else if (op->getName().getStringRef() == "dive_vm.shape_of_activation")
         callee = "DiveVm_GetShapeOfActivation";
+      else if (op->getName().getStringRef() == "dive_vm.cuda_emu_custom_op")
+        callee = "DiveVm_InitializeAndReturnGlobalCustomOpContext";
       else if (op->getName().getStringRef() == "dive_vm.reduction") {
         op->emitError("dive_vm.reduction needs a (float*, float*, int*, int, int, int) TopKVector signature, not the op operand list");
         return signalPassFailure();
@@ -1801,6 +1803,8 @@ struct DwcConvertDiveVmToMemrefPass
         callee = "DiveVm_UpdateDvfsHint";
       else if (op->getName().getStringRef() == "dive_vm.shape_of_activation")
         callee = "DiveVm_GetShapeOfActivation";
+      else if (op->getName().getStringRef() == "dive_vm.cuda_emu_custom_op")
+        callee = "DiveVm_InitializeAndReturnGlobalCustomOpContext";
       else if (op->getName().getStringRef() == "dive_vm.reduction") {
         op->emitError("dive_vm.reduction needs a (float*, float*, int*, int, int, int) TopKVector signature, not the op operand list");
         return signalPassFailure();
